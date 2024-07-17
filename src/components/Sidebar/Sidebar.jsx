c2520e47fbd0bbfa582ea992be08344aa233b39d
Joshua Alvarez <joshuayherrera@gmail.com>
2024-07-17 12:44:56 -0500

    Save prompt in sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,75 +1,70 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import "./Sidebar.css";
 import { assets } from "../../assets/assets";
+import { Context } from "../../context/Context";
+import DarkMode from "./DarkMode";
 
 const Sidebar = () => {
+  const [extended, setExtended] = useState(false);
 
-    const [ extended, setExtended ] = useState(false);
-    return (
-        <div className="sidebar">
-            <div className="top">
-                <img
-                    onClick={() => setExtended(!extended)}
-                    className="menu" 
-                    src={assets.menu_icon} 
-                    alt="menu" 
-                />
-                <div className="new-chat">
-                    <img 
-                        src={assets.plus_icon} 
-                        alt="new-chat" 
-                    />
-                    {extended ? 
-                    (
-                    <p>New Chat</p>
-                    ) : (
-                        null
-                    )}
-                </div>
-                {extended ?
-                (
-                <div className="recent">
-                    <p className="recent-title">
-                        Recent
-                    </p>
-                    <div className="recent-entry">
-                        <img 
-                            src={assets.message_icon}
-                            alt="message" 
-                        />
-                        <p>What is react...</p>
-                    </div>
-                </div>
-                ) : (
-                    null
-                )}
-                
-            </div>
-            <div className="bottom">
-                <div className="bottom-item recent-entry">
-                    <img 
-                        src={assets.question_icon} 
-                        alt="help" 
-                    />
-                    {extended?<p>Help</p>:null}
-                </div>
-                <div className="bottom-item recent-entry">
-                    <img 
-                        src={assets.history_icon} 
-                        alt="history" 
-                    />
-                    {extended?<p>Activity</p>:null}
-                </div>
-                <div className="bottom-item recent-entry">
-                    <img 
-                        src={assets.setting_icon} 
-                        alt="settings" 
-                    />
-                    {extended?<p>Settings</p>:null}
-                </div>
-            </div>
+  const { onSent, prevPrompts, setRecentPrompt, newChat } = useContext(Context);
+
+  const loadPrompt = async (prompt) => {
+    setRecentPrompt(prompt);
+    await onSent(prompt);
+  };
+
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <img
+          onClick={() => setExtended((prev) => !prev)}
+          className="menu"
+          src={assets.menu_icon}
+          alt=""
+        />
+        <div onClick={() => newChat()} className="new-chat">
+          <img src={assets.plus_icon} alt="" />
+          {extended ? <p>New Chat</p> : null}
         </div>
-    )
-}
+        {extended ? (
+          <div className="recent">
+            <p className="recent-title">Recent</p>
+            {prevPrompts.map((item, index) => {
+              return (
+                <div onClick={() => loadPrompt(item)} className="recent-entry">
+                  <img src={assets.message_icon} alt="" />
+                  <p>{item.slice(0, 18)} ...</p>
+                </div>
+              );
+            })}
+          </div>
+        ) : null}
+      </div>
+      <div className="bottom">
+        <div className="bottom-item recent-entry">
+          <img src={assets.question_icon} alt="" />
+          {extended ? <p>Help</p> : null}
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.history_icon} alt="" />
+          {extended ? <p>Activity</p> : null}
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.setting_icon} alt="" />
+          {extended ? <p>Settings</p> : null}
+        </div>
+        <div className="bottom-item recent-entry">
+          <img
+            src="src/assets/darkmode_icon.png"
+            alt=""
+            className="mydarkmodeIcon"
+          />
+          {extended ? <DarkMode /> : null}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Sidebar;
